193e0aefe7ca04ae9198dc717c29d9e1ad4f61f5
Vignesh Varadarajan <vigneshv@gmail.com>
2015-12-13 00:29:12 -0800

    Fixes to make it more usable.

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,7 @@ require 'optparse'
 require 'nokogiri'
 require 'uri'
 require 'date'
-
+require 'rest-client'
 
 def write_xml(xml, options)
   xsl =<<XSL
@@ -47,39 +47,33 @@ Signal.trap("TERM") {
 
 
 optparse = OptionParser.new do |opts|
-  opts.banner = "Usage: scraper.rb [options]"
-
-  opts.on("--url1=URL1", "The URL for the 1st language. (Mandatory)") do |u1|
-    options[:url1] = u1
-  end
-  
-  opts.on("--url2=URL2", "The URL for the 2nd language. (Mandatory)") do |u2|
-    options[:url2] = u2
-  end
-  
-  opts.on("--lang1=LANG1", "The language code for the 1st language. (Mandatory)") do |l1|
-    options[:l1] = l1
-  end
-  
-  opts.on("--lang2=LANG2", "The language code for the 2nd language. (Mandatory)") do |l2|
-    options[:l2] = l2
-  end
+  opts.banner = "Usage: scraper.rb [options] lang1 lang2"
   
   opts.on("-oOUTPUT", "--out=OUTPUT", "The out file name. (Mandatory)") do |o|
     options[:out] = o
   end
   
-  opts.on("-eEND_DATE", "--end=END_DATE", "The end date, format YYYY-MM-DD. (Mandatory)") do |e|
+  opts.on("--date=DATE", "The date range, format YYYY-MM-DD:YYYY-MM-DD. (Mandatory)") do |dr|
+    m = /(.*):(.*)/.match(dr)
+    e = m[2]
+    s = m[1]
     options[:end] = Date.strptime(e, '%Y-%m-%d')
+    options[:start] = Date.strptime(s, '%Y-%m-%d')
     if options[:end].nil?
       options[:end] = Date.today
+    end
+    
+    if options[:start].nil?
+      options[:start] = Date.today
     end
   end
 end
 
+options[:l1] = ARGV[-2]
+options[:l2] = ARGV[-1]
 begin
   optparse.parse!
-  if options[:url1].nil? || options[:url2].nil? || options[:l1].nil? || options[:l2].nil? || options[:out].nil? || options[:end].nil?
+  if options[:l1].nil? || options[:l2].nil? || options[:out].nil?
     raise OptionParser::MissingArgument
   end
 rescue OptionParser::MissingArgument, OptionParser::InvalidOption
@@ -87,10 +81,19 @@ rescue OptionParser::MissingArgument, OptionParser::InvalidOption
   exit
 end
 
-uri1 = URI(options[:url1])
-uri2 = URI(options[:url2])
+base_lang_page1 = Nokogiri::HTML(RestClient.get("http://wol.jw.org/#{options[:l1]}"), nil, 'UTF-8')
+base_lang_page2 = Nokogiri::HTML(RestClient.get("http://wol.jw.org/#{options[:l2]}"), nil, 'UTF-8')
 
-date = Date.new(URI(options[:url1]).path.split('/')[-3].to_i, URI(options[:url1]).path.split('/')[-2].to_i, URI(options[:url1]).path.split('/')[-1].to_i)
+path1 = base_lang_page1.at_css(".todayNav")["href"]
+path2 = base_lang_page2.at_css(".todayNav")["href"]
+
+path1 = path1 << "/#{options[:start].year}/#{options[:start].month}/#{options[:start].day}"
+path2 = path2 << "/#{options[:start].year}/#{options[:start].month}/#{options[:start].day}"
+
+uri1 = URI(File.join("http://wol.jw.org", path1))
+uri2 = URI(File.join("http://wol.jw.org", path2))
+
+date = options[:start]
 to_date = options[:end]
 
 if date >= to_date
@@ -99,14 +102,15 @@ if date >= to_date
 end
 
 
-while date < to_date
+while date <= to_date
   page1 = Nokogiri::HTML(open(uri1.to_s).read, nil, 'UTF-8')
   page2 = Nokogiri::HTML(open(uri2.to_s).read, nil, 'UTF-8')
   
-  date = date + 1
-  puts(date)
   uri1.path = uri1.path.split('/')[0...-3].join("/") << "/#{date.year}/#{date.month}/#{date.day}"
   uri2.path = uri2.path.split('/')[0...-3].join("/") << "/#{date.year}/#{date.month}/#{date.day}"
+  
+  date = date + 1
+  puts(date)
   
   xml_doc.tmx.body.add_child("<tu><tuv xml:lang=\"#{options[:l1]}\"><seg>#{page1.xpath('//*[@class="bodyTxt"]').xpath('.//text()').text.strip}</seg></tuv><tuv xml:lang=\"#{options[:l2]}\"><seg>#{page2.xpath('//*[@class="bodyTxt"]').xpath('.//text()').text.strip}</seg></tuv></tu>")
 end
